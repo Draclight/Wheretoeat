7006a90714575a08091498826375d91352b7dbf8
Rémy Pottiez <rpottiez.pro@gmail.com>
2022-09-05 19:05:32 +0200

    Context

--- WhereToEat/WhereToEat.Data/Data/WhereToEatContext.cs
+++ WhereToEat/WhereToEat.Data/Data/WhereToEatContext.cs
@@ -13,7 +13,8 @@ namespace WhereToEat.Data.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=WhereToEat_dev;Trusted_Connection=True;MultipleActiveResultSets=true");
+            //optionsBuilder.UseSqlServer(@"Server=192.165.85.132;Database=WhereToEat_dev;Trusted_Connection=True;MultipleActiveResultSets=true");
+            optionsBuilder.UseSqlServer(@"Server=192.165.85.132;Database=WhereToEat_dev;User Id=sa;Password=1K37Sp4=@K$E!0gJb`4;");
         }
 
         public DbSet<Role> Roles { get; set; }
